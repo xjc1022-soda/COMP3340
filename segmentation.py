119ce2643426d37dac73ea6d3791dfe83bdf4757
xjc1022-soda <80735003+xjc1022-soda@users.noreply.github.com>
2022-11-08 22:28:10 +0800

    Update segmentation.py

--- segmentation.py
+++ segmentation.py
@@ -1,1 +1,15 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import PIL
+import copy
+import time
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.utils.data as Data
+from torchvision import transforms
+from torchvision.models import vgg19
+
 
